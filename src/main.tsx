5cadee3ec941f3e2050b006ecdc3739d89e1b402
t-kojima <t-kojima@sonicgarden.jp>
2023-07-21 16:48:41 +0900

    yarn devで動くようにする

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
-import './index.css';
 import '@/lib/firebase';
+import { AuthProvider } from './contexts/AuthContext';
+import { UsersProvider } from './contexts/UsersContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AuthProvider>
+      <UsersProvider>
+        <App />
+      </UsersProvider>
+    </AuthProvider>
+  </React.StrictMode>,
 );
