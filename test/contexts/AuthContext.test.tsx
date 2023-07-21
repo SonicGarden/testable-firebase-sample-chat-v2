c0b5c3ce638cde916f3b45ad34ea203d12971086
t-kojima <t-kojima@sonicgarden.jp>
2023-07-21 13:58:01 +0900

    AuthContext.test.tsx にテストを追加

--- test/contexts/AuthContext.test.tsx
+++ test/contexts/AuthContext.test.tsx
@@ -5,6 +5,11 @@ import {
   waitFor,
 } from '@testing-library/react';
 import type { User } from 'firebase/auth';
+import {
+  renderHook,
+  act as actHook,
+  cleanup as cleanupHook,
+} from '@testing-library/react-hooks';
 
 const useAuthStateMock = vi.fn();
 vi.mock('@/hooks/useAuthState', () => {
@@ -46,5 +51,93 @@ describe('AuthProvider', async () => {
         screen.getByText('てすたろうでログインできました')
       ).toBeTruthy()
     );
+  });
+});
+
+const getUserMock = vi.fn();
+const addUserMock = vi.fn();
+vi.mock('@/lib/user', () => {
+  return {
+    getUser: getUserMock,
+    addUser: addUserMock,
+  };
+});
+
+const signInGoogleWithPopupMock = vi.fn();
+const signOutMock = vi.fn();
+vi.mock('@/lib/firebase', async () => {
+  const firebase = await vi.importActual<object>(
+    '@/lib/firebase'
+  );
+  return {
+    ...firebase,
+    signInGoogleWithPopup: signInGoogleWithPopupMock,
+    signOut: signOutMock,
+  };
+});
+
+describe('useAuth', async () => {
+  const { useAuth } = await import('@/contexts/AuthContext');
+   afterEach(async () => {
+    vi.resetAllMocks();
+    await cleanupHook();
+  });
+
+  it('初めてのログインの場合、ユーザー情報が登録される', async () => {
+    const { result } = renderHook(() => useAuth());
+
+    signInGoogleWithPopupMock.mockResolvedValue({
+      user: {
+        uid: 'test-uid',
+        displayName: 'てすたろう',
+        photoURL: null
+      },
+    });
+    getUserMock.mockResolvedValue({ isExist: false });
+    await actHook(async () => {
+      await result.current.signInWithGoogle();
+    });
+
+    expect(addUserMock).toBeCalledWith({
+      uid: 'test-uid',
+      displayName: 'てすたろう',
+      photoURL: null
+    });
+  });
+
+  it('二回目以降のログインの場合、ユーザー情報は登録されない', async () => {
+    const { result } = renderHook(() => useAuth());
+
+    signInGoogleWithPopupMock.mockResolvedValue({
+      user: {
+        uid: 'test-uid',
+        displayName: 'てすたろう',
+        photoURL: null
+      },
+    });
+    getUserMock.mockResolvedValue({ isExist: true });
+    await actHook(async () => {
+      await result.current.signInWithGoogle();
+    });
+
+    expect(addUserMock).not.toBeCalled();
+  });
+
+  it('処理中にエラーが発生した場合はログアウトされる', async () => {
+    const { result } = renderHook(() => useAuth());
+
+    signInGoogleWithPopupMock.mockResolvedValue({
+      user: {
+        uid: 'test-uid',
+        displayName: 'てすたろう',
+        photoURL: null
+      },
+    });
+    getUserMock.mockRejectedValue('error');
+    await actHook(async () => {
+      await result.current.signInWithGoogle();
+    });
+
+    expect(signOutMock).toBeCalled();
   });
 });
