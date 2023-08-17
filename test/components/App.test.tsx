faa6002db7728ec8e7559030c367abd42501b794
hiroro-work <hiroro@sonicgarden.jp>
2023-08-17 17:13:07 +0900

    Appのコンポーネントテストパスするように

--- test/components/App.test.tsx
+++ test/components/App.test.tsx
@@ -1,8 +1,15 @@
 import { render, cleanup, screen } from '@testing-library/react';
-import { App } from '@/components/App';
 
-describe('App', () => {
-  afterEach(() => cleanup());
+vi.mock('@/components/Messages', () => ({ Messages: () => <div>Messages</div> }));
+vi.mock('@/components/MessageForm', () => ({ MessageForm: () => <div>MessageForm</div> }));
+
+describe('App', async () => {
+  const { App } = await import('@/components/App');
+
+  afterEach(() => {
+    vi.clearAllMocks();
+    cleanup();
+  });
 
   it('タイトル文字列が表示される', async () => {
     render(<App />);
