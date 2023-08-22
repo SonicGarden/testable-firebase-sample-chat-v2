47374876a8731fe04a1d848121d89a371e01283a
kontikun <kontikun@sonicgarden.jp>
2023-08-22 16:25:29 +0900

    functionsのテストを追加

---
+++ functions/src/lib/user.ts
@@ -0,0 +1,7 @@
+import { db, CollectionReference } from './firebase';
+import { UserDocumentData } from '../shared/types/user';
+
+export const usersRef = db.collection(
+  'users'
+) as CollectionReference<UserDocumentData>;
+export const userRef = (uid: string) => usersRef.doc(uid);
